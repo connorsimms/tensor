a090007cd914d8eaee369c87f7912c223a117924
Connor Simms <connorvsimms@gmail.com>
2026-01-03 21:15:29 -0600

    shape constructor

--- src/main.cpp
+++ src/main.cpp
@@ -6,10 +6,13 @@ namespace py = pybind11;
 
 PYBIND11_MODULE(tensor, m)
 {
-    m.doc() = "tensor test run";
+    m.doc() = "Tensor";
 
     py::class_<Tensor<float>>(m, "FloatTensor")
-        .def(py::init<>())
-        .def(py::init<std::size_t>())
-        .def("dummy", &Tensor<float>::dummy);
+        .def(py::init<std::vector<std::uint32_t>>())
+        .def("getData", &Tensor<float>::getData)
+        .def("getShape", &Tensor<float>::getShape)
+        .def("getStride", &Tensor<float>::getStride)
+        .def("getInfo", &Tensor<float>::getInfo)
+    ;
 }
--- src/tensor.h
+++ src/tensor.h
@@ -1,17 +1,48 @@
 #pragma once
 
+#include <cstdint>
+#include <functional>
+#include <numeric>
+#include <utility>
 #include <vector>
 
 template <typename T>
 class Tensor
 {
 public:
-    Tensor () : data_{} {}
+    Tensor (const std::vector<std::uint32_t>& shape) 
+    : data_(std::accumulate(shape.begin(), shape.end(), 1, std::multiplies<std::uint32_t>()))
+    , shape_{shape}
+    , stride_(shape.size())
+    {
+        std::transform(
+            shape_.rbegin(), 
+            shape_.rend(), 
+            stride_.rbegin(), 
+            [&, n = 1](const std::uint32_t dim) mutable {
+                auto next = n;
+                n *= dim;
+                return next;
+            }
+        );
+    }
 
-    Tensor (std::size_t size) : data_(size) {}
+    std::vector<T> getData() const { return data_; }
+    std::vector<std::uint32_t> getShape() const { return shape_; }
+    std::vector<std::uint32_t> getStride() const { return stride_; }
 
-    std::string dummy() { return std::string("Hello World\n"); }
+    std::string getInfo()
+    {
+        std::string info {};
+        for (auto d: shape_) info += std::to_string(d) + " ";
+        info += "\n";
+        for (auto d: stride_) info += std::to_string(d) + " "; 
+        info += "\n";
+        return info;
+    }
 
 private:
     std::vector<T> data_;
+    std::vector<std::uint32_t> shape_;
+    std::vector<std::uint32_t> stride_;
 };
